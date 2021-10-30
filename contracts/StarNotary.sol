86612ffb4638e0ae9c53871bf4c144bbab44b265
git-sid-go <siddhuvaradan@gmail.com>
2021-10-30 19:36:44 +0530

    feat: update method to exchange starts

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -71,6 +71,11 @@ contract StarNotary is ERC721 {
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
         //4. Use _transferFrom function to exchange the tokens.
+        require(msg.sender == ownerOf(_tokenId1) || msg.sender == ownerOf(_tokenId2), "You do not own either of the star involved in the exchange.");
+        address starOwner1 = ownerOf(_tokenId1);
+        address starOwner2 = ownerOf(_tokenId2);
+        _transferFrom(starOwner1, starOwner2, _tokenId1);
+        _transferFrom(starOwner2, starOwner1, _tokenId2);
     }
 
     // Implement Task 1 Transfer Stars
