5fb585cd63cdfd92688afea5ab96c799f800ad3f
git-sid-go <siddhuvaradan@gmail.com>
2021-10-31 13:32:46 +0530

    feat: setup for deployment on the rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const infuraKey = "<INFURA KEY>";
 //
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const mnemonic = "<METAMASK SEED WORDS>";
 
 module.exports = {
   /**
@@ -50,6 +50,17 @@ module.exports = {
       host: "127.0.0.1", // Localhost (default: none)
       port: 9545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
+    },
+
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${infuraKey}`
+        ),
+      network_id: 4, // rinkeby's id
+      gas: 4500000, // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000,
     },
 
     // Another network with more advanced options...
